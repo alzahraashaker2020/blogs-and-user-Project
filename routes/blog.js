23f4faf85cd5046f9ab7c81aac013702e6253850
alzahraashaker2020 <77900055+alzahraashaker2020@users.noreply.github.com>
2021-01-25 12:29:45 +0200

    Update blog.js

--- routes/blog.js
+++ routes/blog.js
@@ -7,7 +7,7 @@ const multer=require('multer');
 const path=require('path');
 const storage = multer.diskStorage({
     destination:function(req,file,cb){
-        cb(null, '/static');
+        cb(null, 'static/');
     },
     filename: function(req, file, cb) {
         cb(null, file.originalname + '-' + Date.now() + path.extname(file.originalname));
