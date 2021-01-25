7f023e3ac69f6076d6de2d4907be4cc3c78388dd
alzahraashaker2020 <77900055+alzahraashaker2020@users.noreply.github.com>
2021-01-25 02:36:46 +0200

    Update blog.js

--- routes/blog.js
+++ routes/blog.js
@@ -99,7 +99,7 @@ router.get('/tags/:tags', async (req, res, next) => {
     }
 });
 //delete by id
-router.get('/deletes/:id', async (req, res, next) => {
+router.delete('/deletes/:id', async (req, res, next) => {
     const { params: { id } } = req;
     try {
         const blog = await deleteBlog(id);
